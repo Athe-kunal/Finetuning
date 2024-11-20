0ddf57783e2c0383614f185c50a6bb7f4fb78cc6
Athe-kunal <athekunal@gmail.com>
2024-11-19 23:49:49 -0800

    clean the run file

--- finetuning/finetuning/run.py
+++ finetuning/finetuning/run.py
@@ -1,6 +1,5 @@
-import subprocess
-import os
 from finetune import run_evaluation, run_finetune
+
 LLAMA_3_2_1B_INSTRUCT = "unsloth/Llama-3.2-1B-Instruct"
 LLAMA_3_2_3B_INSTRUCT = "unsloth/Llama-3.2-3B-Instruct"
 LLAMA_3_1_8B_INSTRUCT = "unsloth/llama-3-8b-Instruct"
@@ -9,23 +8,21 @@ MISTRAL_MODEL = "unsloth/mistral-7b-instruct-v0.3"
 
 
 if __name__ == "__main__":
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", MISTRAL_MODEL, "--dataset_name", "bfcl", "--json_or_yaml", "json"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_1B_INSTRUCT, "--dataset_name", "bfcl", "--json_or_yaml", "yaml"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_3B_INSTRUCT, "--dataset_name", "bfcl", "--json_or_yaml", "json"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_3B_INSTRUCT, "--dataset_name", "bfcl", "--json_or_yaml", "yaml"])
+    # run_finetune(model_name=LLAMA_3_2_1B_INSTRUCT, dataset_name="bfcl", json_or_yaml="yaml")
+    # run_finetune(model_name=LLAMA_3_2_3B_INSTRUCT, dataset_name="bfcl", json_or_yaml="json")
+    # run_finetune(model_name=LLAMA_3_2_3B_INSTRUCT, dataset_name="bfcl", json_or_yaml="yaml")
     
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_1_8B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "yaml"])
+    # run_finetune(model_name=LLAMA_3_1_8B_INSTRUCT, dataset_name="xlam", json_or_yaml="yaml")
     
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_1B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "json"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_1B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "yaml"])
+    # run_finetune(model_name=LLAMA_3_2_1B_INSTRUCT, dataset_name="xlam", json_or_yaml="json")
+    # run_finetune(model_name=LLAMA_3_2_1B_INSTRUCT, dataset_name="xlam", json_or_yaml="yaml")
 
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_3B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "json"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_2_3B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "yaml"])
+    # run_finetune(model_name=LLAMA_3_2_3B_INSTRUCT, dataset_name="xlam", json_or_yaml="json")
+    # run_finetune(model_name=LLAMA_3_2_3B_INSTRUCT, dataset_name="xlam", json_or_yaml="yaml")
 
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_1_8B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "json"])
-    # subprocess.run(["poetry", "run", "python", "finetune.py", "--model_name", LLAMA_3_1_8B_INSTRUCT, "--dataset_name", "xlam", "--json_or_yaml", "yaml"])
+    # run_finetune(model_name=LLAMA_3_1_8B_INSTRUCT, dataset_name="xlam", json_or_yaml="json")
+    # run_finetune(model_name=LLAMA_3_1_8B_INSTRUCT, dataset_name="xlam", json_or_yaml="yaml")
     
-    # subprocess.run(["poetry", "run", "python", "eval.py", "--model_name", LLAMA_3_2_1B_INSTRUCT,"--path", "outputs_unsloth/Llama-3.2-1B-Instruct_json_bfcl", "--dataset_name", "bfcl", "--json_or_yaml", "json"])
     # run_evaluation(model_path="outputs_unsloth/Llama-3.2-1B_json_bfcl/epoch_1", model_name=LLAMA_3_2_1B_INSTRUCT, json_or_yaml="json", dataset_name="bfcl")
     # run_evaluation(model_path="outputs_unsloth/Llama-3.2-1B-Instruct_json_bfcl/epoch_2", model_name=LLAMA_3_2_1B_INSTRUCT, json_or_yaml="json", dataset_name="bfcl")
     # run_evaluation(model_path="outputs_unsloth/Llama-3.2-1B-Instruct_json_bfcl/epoch_3", model_name=LLAMA_3_2_1B_INSTRUCT, json_or_yaml="json", dataset_name="bfcl")
